85b95c74efe169aaa94ea53ba3115d6a085b08ef
Administrator <admin@example.com>
2023-11-17 11:37:48 +0900

    init

---
+++ README.md
@@ -0,0 +1,75 @@
+# NDFC Template CLI
+A Command Line Interface designed for listing and downloading NDFC Templates from Cisco NDFC
+
+## Installation
+
+Clone the repo
+```bash
+git clone https://github.com/insobi/ndfc-template-downloader.git
+```
+Go to your project folder
+```bash
+cd ndfc-template-downloader
+```
+
+Install dependencies
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Set environment variables
+```bash
+export NDFC_USERNAME="admin"
+export NDFC_PASSWORD="C1sco12345"
+export ND_URL="https://198.18.133.100"
+```
+
+Display how to use
+```bash
+Usage: ndfc-template.py [OPTIONS] COMMAND [ARGS]...
+
+  Retrieve GitLab project data and retry pripeline jobs
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  get   Download NDFC template(s)
+  list  Display a list of template from NDFC
+```
+
+check out list of TEMPLATE_CLI based templates from NDFC
+```bash
+$ python ndfc-template.py list --template-cli
++-----------------------------------------------+--------------+---------------------------+--------------+-------------------------------------------+
+| name                                          | templateType | templateSubType           | contentType  | supportedPlatforms                        |
++-----------------------------------------------+--------------+---------------------------+--------------+-------------------------------------------+
+| Default_Network_Extension_Universal           | PROFILE      | VXLAN                     | TEMPLATE_CLI | All                                       |
+| Default_Network_Universal                     | PROFILE      | VXLAN                     | TEMPLATE_CLI | All                                       |
+| Default_VRF_Extension_Universal               | PROFILE      | VXLAN                     | TEMPLATE_CLI | All                                       |
+| Default_VRF_Universal                         | PROFILE      | VXLAN                     | TEMPLATE_CLI | All                                       |
+| External_VRF_Lite_eBGP                        | POLICY       | DEVICE                    | TEMPLATE_CLI | All                                       |
+| Service_Network_Universal                     | PROFILE      | SERVICE                   | TEMPLATE_CLI | N9K                                       |
+...
+
+```
+
+Or check out specific template by name
+```bash
+$ python ndfc-template.py list --name int_trunk_host
++----------------+--------------+--------------------+-------------+--------------------+
+| name           | templateType | templateSubType    | contentType | supportedPlatforms |
++----------------+--------------+--------------------+-------------+--------------------+
+| int_trunk_host | POLICY       | INTERFACE_ETHERNET | PYTHON      | All                |
++----------------+--------------+--------------------+-------------+--------------------+
+```
+
+Download templates from NDFC
+```bash
+$ python ndfc-template.py get --name int_trunk_host
+
+$ ls -l int_trunk_host*
+-rw-r--r--  1 insekim  staff  12466 Nov 17 11:24 int_trunk_host.template
+```
---
+++ ndfc-template.py
@@ -0,0 +1,142 @@
+import requests
+import os
+from urllib3.exceptions import InsecureRequestWarning
+import click
+from prettytable import PrettyTable
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+class ndfcTemplate(object):
+    def __init__(self):
+        self.base_url = ""
+        self.username = ""
+        self.password = ""
+        self.ssl_verify = False
+        self.headers = {"Content-Type": "text/plain"}
+
+    def login(self, user, pw, url):
+        self.base_url = url
+        self.username = user
+        self.password = pw
+        payload = f'{{ "userName": "{self.username}", "userPasswd": "{self.password}", "domain": "local" }}'
+        response = requests.request(
+            'POST',
+            f'{self.base_url}/login',
+            headers=self.headers,
+            data=payload,
+            verify=self.ssl_verify
+        )
+        self.headers["Cookie"] = f'AuthCookie={response.json()["jwttoken"]}'
+
+    def list(self, name:str) -> list:
+        template_list = []
+        response = requests.request(
+            "GET",
+            f'{self.base_url}/appcenter/cisco/ndfc/api/v1/configtemplate/rest/config/templates/{name}',
+            headers=self.headers,
+            verify=self.ssl_verify
+        )
+        if response.status_code == 200:
+            if name == '':
+                for item in response.json():
+                    print(item)
+                    template_list.append(item)
+            else:
+                template_list.append(response.json())
+        return template_list
+
+    def get(self, template_name: str, path: str) -> None:
+        response = requests.request(
+            "GET",
+            f'{self.base_url}/appcenter/cisco/ndfc/api/v1/configtemplate/rest/config/templates/{template_name}',
+            headers=self.headers,
+            verify=self.ssl_verify
+        )
+        content = response.json()["content"]
+        with open(f"{path}/{template_name}.template", "w") as f:
+            f.write(content)
+
+@click.group()
+@click.pass_context
+def ndfc_template(ctx):
+    '''
+    Retrieve GitLab project data and retry pripeline jobs
+    '''
+    if "NDFC_USERNAME" not in os.environ or "NDFC_PASSWORD" not in os.environ or "ND_URL" not in os.environ:
+        click.secho(
+            "ERROR: You must specify NDFC_USERNAME and NDFC_PASSWORD and ND_URL as environment variables.",
+            fg="red"
+        )
+        exit(1)
+
+    obj = ndfcTemplate()
+    obj.login(user=os.environ['NDFC_USERNAME'],
+              pw=os.environ['NDFC_PASSWORD'], url=os.environ['ND_URL'])
+    ctx.obj = obj
+
+@click.command()
+@click.pass_obj
+@click.option("--name", type=str, required=False, default='', help='template name')
+@click.option("--all", 'type', flag_value='all', default=True)
+@click.option("--template-cli", 'type', flag_value='TEMPLATE_CLI', default=True)
+@click.option("--python", 'type', flag_value='PYTHON', default=True)
+def list(obj, name, type):
+    '''Display a list of template from NDFC'''
+    table = PrettyTable()
+    table.field_names = [
+        "name",
+        "templateType",
+        "templateSubType",
+        "contentType",
+        "supportedPlatforms"
+    ]
+    table.align = "l"
+    for item in obj.list(name=name):
+        row = [
+            item["name"],
+            item["templateType"],
+            item["templateSubType"],
+            item["contentType"],
+            item["supportedPlatforms"]
+        ]
+        if type != 'all':
+            if item["contentType"] == type:
+                table.add_row(row)
+        else:
+            table.add_row(row)
+    click.echo(table)
+
+@click.command()
+@click.pass_obj
+@click.option("--name", type=str, required=False, help='name of template to download')
+@click.option("--all", 'all', flag_value=True, default=False, help='download all templates')
+@click.option("--path", type=str, required=True, default=".", help='path for downloading files')
+def get(obj, name, all, path):
+    '''Download NDFC template(s)'''
+    if not name and not all:
+        print("Error: Command 'get' requires arguments: --all or --name")
+        exit(1)
+    if not all:
+        obj.get(name, path)
+    elif all:
+        templates = []
+        for template in obj.template_list():
+            obj.get(template['name'])
+
+@click.command()
+@click.pass_obj
+@click.option("--name", type=str, required=False)
+@click.option("--file", type=str, required=True, default=".")
+def put(obj, name, file) -> None:
+    '''Upload a template to NDFC'''
+    if not name:
+        print("Error: Command 'put' requires arguments: --name")
+        exit(1)
+    else:
+        obj.put(name, file)
+
+ndfc_template.add_command(list)
+ndfc_template.add_command(get)
+
+if __name__ == "__main__":
+    ndfc_template()
